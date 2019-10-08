5153df5b42a0b1c4c42ad34f10be81e45889c7d8
dutyujm <727124697@qq.com>
2019-10-08 16:26:46 +0800

    Initial commit

--- blogger/src/main/java/cn/dutyujm/controller/AdminController.java
+++ blogger/src/main/java/cn/dutyujm/controller/AdminController.java
@@ -1,4 +1,12 @@
 package cn.dutyujm.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/Admin")
 public class AdminController {
+
 }
